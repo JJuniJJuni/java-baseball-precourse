cc2b6282ba41d1afbc9bd9f74323debaf57e3559
JJuniJJuni <qkrgud305@naver.com>
2021-10-06 12:52:05 +0900

    feat: Main 함수에 게임 시작 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import baseball.controller.BaseBallController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
-    }
+	public static final BaseBallController baseBallController = new BaseBallController();
+
+	public static void main(String[] args) {
+		// TODO 숫자 야구 게임 구현
+		baseBallController.run();
+	}
 }
